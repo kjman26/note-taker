a95f94679b111536212de372f0e09975db69a33e
Chris <kjman26@gmail.com>
2022-11-09 18:04:36 -0800

    second initial commit

--- Assets/generateHtrml.js
+++ Assets/generateHtrml.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000;
 const app = express();
-const index = require('./')
+const index = require('')
 
 app.get((res, req) => {
     
